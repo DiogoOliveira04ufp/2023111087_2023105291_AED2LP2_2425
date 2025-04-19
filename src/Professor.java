39c6a48a0f27c400ed8c8e2745a0ba287d282303
Diogo Oliveira <2023111087@ufp.edu.pt>
2025-04-19 21:27:39 +0100

    v20250419.1 >adições à classe Professor

--- src/Professor.java
+++ src/Professor.java
@@ -2,12 +2,28 @@ import java.util.ArrayList;
 
 public class Professor extends Pessoa
 {
+    private final int numero;
+    private String qualificacao;
 
-
-    public Professor(String nome, String email, ArrayList<Cadeira> cadeiras, Horario horario)
+    public Professor(String nome, String email, ArrayList<Cadeira> cadeiras, Horario horario, int numero, String qualificacao)
     {
         super(nome, email, horario, cadeiras);
-
+        this.numero = numero;
+        this.qualificacao = qualificacao;
     }
 
+    public void setQualification(String qualification)
+    {
+        this.qualificacao = qualification;
+    }
+
+    public int getProfNumber()
+    {
+        return this.numero;
+    }
+
+    public String getQualification()
+    {
+        return this.qualificacao;
+    }
 }
